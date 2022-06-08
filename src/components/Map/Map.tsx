8ec40949b1ab52227b345c253963eead7866cb5c
karl <karol010k@gmail.com>
2022-06-08 18:42:07 +0200

    Map and markers are finished.

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {MapContainer, Marker, Popup, TileLayer} from 'react-leaflet';
 import '../../utils/fix-map-icon'
+import {SearchContext} from "../../contexts/search.context";
+import {SimpleAdEntity} from 'types';
 
 import 'leaflet/dist/leaflet.css';
 import './Map.css'
+import {SingleAd} from "./SingleAd";
+
 
 export const Map = () => {
+    const {search} = useContext(SearchContext);
+    const [ads, setAds] = useState<SimpleAdEntity[]>([]);
+
+
+  useEffect(()=>{
+      (async ()=>{
+
+         const res = await fetch(`http://localhost:3001/ad/search/${search}`)
+          const data = await res.json();
+
+         setAds(data);
+
+      })();
+  },[search]);
+
     return (
       <div className="map">
       <MapContainer center={[52.4040697,16.9137443]} zoom={13}>
@@ -13,12 +32,16 @@ export const Map = () => {
              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
              attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap<a/> & contributors"
           />
-          <Marker position={[52.4040697,16.9137443]}>
-              <Popup>
-                  <h2>Poznan</h2>
-                  <p>Smierdzi kupa</p>
-              </Popup>
-          </Marker>
+
+          {
+              ads.map(ad=>(
+                  <Marker key={ad.id} position={[ad.lat,ad.lon]}>
+                      <Popup>
+                          <SingleAd id={ad.id}/>
+                      </Popup>
+                  </Marker>
+              ))
+          }
       </MapContainer>
       </div>
     );
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, {SyntheticEvent, useContext, useState} from "react";
 import { Btn } from "../common/Btn";
 import './Header.css';
+import {SearchContext} from "../../contexts/search.context";
 
-export const Header = () => (
+
+export const Header = () => {
+    const {search,setSearch} = useContext(SearchContext);
+    const [inputVal,setInputVal] = useState(search);
+
+    const setSearchFromLocalState = (e: SyntheticEvent) => {
+        e.preventDefault();
+      setSearch(inputVal);
+    };
+
+    return (
     <header>
         <h1>
             <strong>Mega</strong> Ogłoszenie
         </h1>
         <Btn text="Dodaj Ogłoszenie"/>
-        <div className="search">
-            <input type="text"/> <Btn text="Szukaj"/>
-        </div>
+        <form className="search" onSubmit={setSearchFromLocalState}>
+            <input type="text" value={inputVal} onChange={e => setInputVal(e.target.value)}/> <Btn text="Szukaj"/>
+        </form>
     </header>
-);
+    );
+};
